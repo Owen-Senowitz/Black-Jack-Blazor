c63499aed491ca960168d520612fb08d89b6c19d
Owen Senowitz <owen.senowitz@gmail.com>
2022-03-24 19:00:43 -0400

    Add project files.

---
+++ Black Jack Blazor/Data/BlackJack/Card.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Jack
+{
+    public enum SuitType
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+    public enum CardValueType
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+    public class Card
+    {
+
+        public SuitType Suit { get; set; }
+        public CardValueType CardValue { get; set; }
+        public int Score
+        {
+            get
+            {
+                if (CardValue == CardValueType.Ace)
+                {
+                    return 11;
+                }
+                if (CardValue == CardValueType.King || CardValue == CardValueType.Queen || CardValue == CardValueType.Jack)
+                {
+                    return 10;
+                }
+                if (CardValue == CardValueType.Ten)
+                {
+                    return 10;
+                }
+                if (CardValue == CardValueType.Nine)
+                {
+                    return 9;
+                }
+                if (CardValue == CardValueType.Eight)
+                {
+                    return 8;
+                }
+                if (CardValue == CardValueType.Seven)
+                {
+                    return 7;
+                }
+                if (CardValue == CardValueType.Six)
+                {
+                    return 6;
+                }
+                if (CardValue == CardValueType.Five)
+                {
+                    return 5;
+                }
+                if (CardValue == CardValueType.Four)
+                {
+                    return 4;
+                }
+                if (CardValue == CardValueType.Three)
+                {
+                    return 3;
+                }
+                if (CardValue == CardValueType.Two)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public Card(SuitType suit, CardValueType cardvalue)
+        {
+            Suit = suit;
+            CardValue = cardvalue;
+        }
+        public override string ToString()
+        {
+            return $"{CardValue} of {Suit}";
+        }
+    }
+}
---
+++ Black Jack Blazor/Data/BlackJack/Deck.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Jack
+{
+    public class Deck
+    {
+        public List<Card> deck { get; set; }
+        private int currentCard;
+        private const int maxCards = 52;
+        private Random ranNum;
+
+        public Deck()
+        {
+            deck = new List<Card>();
+            currentCard = 0;
+
+            int x = 0;
+            for (int i = 0; i < Enum.GetValues(typeof(SuitType)).Length; i++)
+            {
+                for (int j = 0; j < Enum.GetValues(typeof(CardValueType)).Length; j++, x++)
+                {
+                    var card = new Card((SuitType) i, (CardValueType) j);
+                    deck.Add(card);
+                }
+            }
+        }
+
+        public void printDeck()
+        {
+            foreach (var card in deck)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+        public void Shuffle()
+        {
+            ranNum = new Random();
+            int n = deck.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = ranNum.Next(n + 1);
+                Card value = deck[k];
+                deck[k] = deck[n];
+                deck[n] = value;
+            }
+        }
+        public Card DealCard()
+        {
+            if (currentCard < deck.Count)
+            {
+                return deck[currentCard++];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
